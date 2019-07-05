1611c06365feb08513a73cef351f38ba828ea4fa
César Carruitero <ccarruitero@protonmail.com>
2019-07-05 15:45:05 -0500

    add draw canvas

---
+++ src/draw.rs
@@ -0,0 +1,68 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+use std::cell::Cell;
+use std::rc::Rc;
+use wasm_bindgen::JsCast;
+
+extern crate serde_derive;
+
+#[wasm_bindgen]
+pub fn start() -> Result<(), JsValue> {
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    document.body().unwrap().append_child(&canvas)?;
+    let width = window.inner_width().unwrap().into_serde().unwrap();
+    let height = window.inner_height().unwrap().into_serde().unwrap();
+    canvas.set_width(width);
+    canvas.set_height(height);
+    // canvas.style().set_property("border", "solid")?;
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if pressed.get() {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
+    console::log_1(&JsValue::from_str("drawing canvas from rust!"));
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
+mod draw;
 
+use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -18,10 +18,6 @@ pub fn main_js() -> Result<(), JsValue> {
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
-
+    draw::start();
     Ok(())
 }
